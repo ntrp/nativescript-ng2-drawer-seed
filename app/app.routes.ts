832bc543c6806863cce4a63eba2487eec3e8287c
Ivan Greguric Ortolan <ivan.g.ortolan@gmail.com>
2016-07-16 15:49:07 +0200

    added a drawerless page

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -2,11 +2,13 @@ import {RouterConfig} from '@angular/router';
 import {nsProvideRouter} from "nativescript-angular/router";
 
 import {HomeComponent} from './components/home';
+import {TodoComponent} from './components/todo';
 import {SettingsComponent} from './components/settings';
 
 const routes:RouterConfig = [
     {path: "", redirectTo: "/home", terminal: true},
     {path: "home", component: HomeComponent},
+    {path: "todo", component: TodoComponent},
     {path: "settings", component: SettingsComponent},
 ];
 
--- app/components/settings/settings.component.ts
+++ app/components/settings/settings.component.ts
@@ -1,20 +1,21 @@
-import {Component, ChangeDetectorRef} from '@angular/core';
+import {Component} from '@angular/core';
 import {Location} from '@angular/common';
 import {Router} from '@angular/router';
 
 import {NS_ROUTER_DIRECTIVES_EXT} from '../../shared/router/ns-router-ext';
-import {PageComponent} from '../page.component';
-import {DrawerComponent} from '../drawer/drawer.component';
 
 @Component({
     moduleId: module.id,
     selector: 'settings',
     templateUrl: './settings.component.html',
-    directives: [NS_ROUTER_DIRECTIVES_EXT, DrawerComponent]
+    directives: [NS_ROUTER_DIRECTIVES_EXT]
 })
-export class SettingsComponent extends PageComponent {
+export class SettingsComponent {
 
-    constructor(protected changeDetectionRef:ChangeDetectorRef, protected router:Router, protected location:Location) {
-        super(changeDetectionRef, router, location);
+    constructor(protected router:Router, protected location:Location) {
+    }
+
+    goBack() {
+        this.location.back();
     }
 }
