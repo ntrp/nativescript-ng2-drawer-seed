fe7919ee1efad0d22c1c4ab69e2443b4c2e1fc3b
Ivan Greguric Ortolan <ivan.g.ortolan@gmail.com>
2016-07-16 18:57:00 +0200

    added working ddpclient, need to implement angular2 integration

--- app/index.ts
+++ app/index.ts
@@ -1,7 +1,6 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import {nativeScriptBootstrap} from 'nativescript-angular/application';
 // angular2 and nativescript
-import {provide} from '@angular/core';
 import {RouterOutletMap} from '@angular/router';
 import {NS_ROUTER_PROVIDERS} from 'nativescript-angular/router';
 // telerik UI
@@ -15,6 +14,47 @@ import {APP_ROUTES_PROVIDER} from './app.routes';
 import * as native from './utils/native';
 
 native.StatusBar.setColor('#388e3c');
+
+var DDPClient = require('nativescript-meteor');
+
+var ddpclient = new DDPClient({
+    host: "localhost",
+    port: 3000,
+    ssl: false,
+    autoReconnect: true,
+    autoReconnectTimer: 15000,
+    maintainCollections: true,
+    ddpVersion: '1',
+    useSockJs: true
+});
+
+ddpclient.connect(function (error, wasReconnect) {
+    if (error) {
+        console.log('DDP connection error!');
+        return;
+    }
+
+    if (wasReconnect) {
+        console.log('Reestablishment of a connection.');
+    }
+
+    console.log('connected!');
+
+    // SUBSCRIBE COLLECTIONS
+    ddpclient.subscribe(
+        'todos',                  // name of Meteor Publish function to subscribe to
+        [],                       // any parameters used by the Publish function
+        function () {             // callback when the subscription is complete
+            console.log('posts complete:');
+            console.log(ddpclient.collections.todos);
+            for (var _id in ddpclient.collections.todos) {
+                var todos = ddpclient.collections.todos[_id];
+                console.log("Adding available todos " + _id + " name: " + todos.judul);
+            }
+        });
+});
+/*
+*/
 
 nativeScriptBootstrap(
     AppComponent,
